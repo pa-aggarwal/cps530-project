44a57950cde6500ff1b918e76fd570e11e71c6bf
Priya Aggarwal <priya.aggarwal0125@gmail.com>
2021-11-29 18:12:05 -0500

    Add hero section of demo page and custom styles

--- app.py
+++ app.py
@@ -6,3 +6,7 @@ app = Flask(__name__)
 @app.route("/home")
 def index():
     return render_template("index.html")
+
+@app.route("/demo")
+def demo():
+    return render_template("demo.html", title="Demo")
